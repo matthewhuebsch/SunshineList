ba5e25bdd6ad02b1d3e40a6724d1cee54f633f8b
matthewhuebsch <matthew.huebsch+github@gmail.com>
2016-07-31 14:42:07 -0400

    MH. Wrangle pre-2015 universities data. Add per-year university section to the report.

--- data_scape.R
+++ data_scape.R
@@ -6,8 +6,15 @@ for (i in 1:16){
   tables <- readHTMLTable(theurl)
   n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
   tables[[which.max(n.rows)]]
-  write.csv(x = tables, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2013", paste0("page ", i, ".csv")))
+  names(tables[[1]]) = c("Employer", "Last.name", "First.name", "Job.title", "Salary.Paid", "Taxable.Benefits")
+  tables[[1]]$Calendar.Year = 2013
+  tables[[1]]$Sector = "Universities"
+  write.csv(x = tables[[1]], file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2013", paste0("page ", i, ".csv")))
 }
+
+filenames = list.files(file.path(getwd(), "Inputs", "Salary Disclosure by year", "2013"), full.names = TRUE)
+bound_file = do.call("rbind", lapply(filenames, read.csv, header=TRUE))
+write.csv(x = bound_file, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "Curated Data", "2013", "merged_file.csv"))
 
 # for 2012
 for (i in 1:15){
@@ -15,8 +22,16 @@ for (i in 1:15){
   tables <- readHTMLTable(theurl)
   n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
   tables[[which.max(n.rows)]]
-  write.csv(x = tables, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2012", paste0("page ", i, ".csv")))
+  names(tables[[1]]) = c("Employer", "Last.name", "First.name", "Job.title", "Salary.Paid", "Taxable.Benefits")
+  tables[[1]]$Calendar.Year = 2012
+  tables[[1]]$Sector = "Universities"
+  write.csv(x = tables[[1]], file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2012", paste0("page ", i, ".csv")))
 }
+
+filenames = list.files(file.path(getwd(), "Inputs", "Salary Disclosure by year", "2012"), full.names = TRUE)
+bound_file = do.call("rbind", lapply(filenames, read.csv, header=TRUE))
+write.csv(x = bound_file, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "Curated Data", "2012", "merged_file.csv"))
+
 
 
 # for 2011
@@ -27,29 +42,57 @@ for (i in 1:12){
   tables <- readHTMLTable(theurl)
   n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
   tables[[which.max(n.rows)]]
-  write.csv(x = tables, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2011", paste0("page ", i, ".csv")))
+  names(tables[[1]]) = c("Employer", "Last.name", "First.name", "Job.title", "Salary.Paid", "Taxable.Benefits")
+  tables[[1]]$Calendar.Year = 2011
+  tables[[1]]$Sector = "Universities"
+  write.csv(x = tables[[1]], file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2011", paste0("page ", i, ".csv")))
 }
 
+filenames = list.files(file.path(getwd(), "Inputs", "Salary Disclosure by year", "2011"), full.names = TRUE)
+bound_file = do.call("rbind", lapply(filenames, read.csv, header=TRUE))
+write.csv(x = bound_file, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "Curated Data", "2011", "merged_file.csv"))
+
+
+
 # for 2010
-v = c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l")
+v = c("a", "b")
 
 for (i in 1:2){
   theurl <- paste0("http://www.fin.gov.on.ca/en/publications/salarydisclosure/2011/univer11", v[i], ".html")
   tables <- readHTMLTable(theurl)
   n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
   tables[[which.max(n.rows)]]
-  write.csv(x = tables, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2010", paste0("page ", i, ".csv")))
+  names(tables[[1]]) = c("Employer", "Last.name", "First.name", "Job.title", "Salary.Paid", "Taxable.Benefits")
+  tables[[1]]$Calendar.Year = 2010
+  tables[[1]]$Sector = "Universities"
+  write.csv(x = tables[[1]], file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2010", paste0("page ", i, ".csv")))
 }
+
+filenames = list.files(file.path(getwd(), "Inputs", "Salary Disclosure by year", "2010"), full.names = TRUE)
+bound_file = do.call("rbind", lapply(filenames, read.csv, header=TRUE))
+write.csv(x = bound_file, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "Curated Data", "2010", "merged_file.csv"))
+
+
 
 
 
 # for 2009
-v = c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l")
+v = c("a", "b")
 
 for (i in 1:2){
   theurl <- paste0("http://www.fin.gov.on.ca/en/publications/salarydisclosure/2010/univer10", v[i], ".html")
   tables <- readHTMLTable(theurl)
   n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
   tables[[which.max(n.rows)]]
-  write.csv(x = tables, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2009", paste0("page ", i, ".csv")))
+  names(tables[[1]]) = c("Employer", "Last.name", "First.name", "Job.title", "Salary.Paid", "Taxable.Benefits")
+  tables[[1]]$Calendar.Year = 2009
+  tables[[1]]$Sector = "Universities"
+  write.csv(x = tables[[1]], file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "2009", paste0("page ", i, ".csv")))
 }
+
+filenames = list.files(file.path(getwd(), "Inputs", "Salary Disclosure by year", "2009"), full.names = TRUE)
+bound_file = do.call("rbind", lapply(filenames, read.csv, header=TRUE))
+write.csv(x = bound_file, file = file.path(getwd(), "Inputs", "Salary Disclosure by year", "Curated Data", "2009", "merged_file.csv"))
+
+
+
